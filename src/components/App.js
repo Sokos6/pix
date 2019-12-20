2b4a307482b5329dede520e15b2690b0b7a15c41
Sokos6 <will.sokolowski@gmail.com>
2019-12-20 01:42:57 -0600

    axios api

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID 27510fc89b72a0f5c87ccb97cf6f42a480489301d8b4373f261bebca5b90b50e'
+      }
+    });
   }
 
   render() {
