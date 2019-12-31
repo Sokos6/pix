1a45cee02bf9d791bcdf5ff84b3655e656aa23c2
Sokos6 <will.sokolowski@gmail.com>
2019-12-31 00:12:08 -0600

    imagecard

---
+++ src/components/ImageCard.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    console.log(this.imageRef);
+  }
+
+  render() {
+    const { description, urls } = this.props.image;
+    return (
+      <div>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
+      </div>
+    );
+  }
+}
+
+export default ImageCard;
